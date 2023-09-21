36caa7158b48372693128f58386d09755a8d6ab7
JuanManuelLopez <26lopez.jm@gmail.com>
2023-09-21 10:52:40 -0300

    yatusabe

--- src/components/Carrito/CarritoBotones.jsx
+++ src/components/Carrito/CarritoBotones.jsx
@@ -3,15 +3,27 @@ import React, { useState } from 'react'
 const CarritoBotones = (props) => {
   console.log(props.contexto.Carrito)
   const [HayMotos, setHayMotos] = useState(false)
-  
+  let probar
+  const armarTexto = () => {
+    props.contexto.Carrito.map(item=>{
+      if (probar===undefined && props.contexto.Carrito.indexOf(item)===0) {
+        probar= `Estoy%20interesado/a%20en%20financiar%20${item.Clase==='Motos' ? 'una' : 'un'}%20${item.brand}%20${item.model}%20${item.Clase==='Cascos' ? item.design : ''}`
+      }else if(props.contexto.Carrito.indexOf(item)+1===props.contexto.Carrito.length){
+        probar= probar + `%20y%20${item.Clase==='Motos' ? 'una' : 'un'}%20${item.brand}%20${item.model}%20${item.Clase==='Cascos' ? item.design : ''}`
+      }else{
+        probar= probar + `%2C%20${item.Clase==='Motos' ? 'una' : 'un'}%20${item.brand}%20${item.model}%20${item.Clase==='Cascos' ? item.design : ''}`
+      }
+    })
+  } 
+  armarTexto()
   return (
     <div id='divTotalyBotones'>
     <h3 style={{ color: 'white', marginBottom:'5%'}}>Total: U$S {props.contexto.TotalCash}</h3>
-    <div style={{ width: '100%', display: 'flex', flexDirection:props.contexto.Orientation==='Portrait' ? 'row' : 'column', justifyContent: 'space-around', alignItems: 'center' }}>
+    <div style={{ width: '100%'}}>
     {
       props.contexto.Carrito.find(item=>item.Clase==='Motos')
       ?
-      <button onClick={()=>{props.HandleModal()}} className='BotonesProductoWallpaper' id='BotonesProductoWallpaperWp' >Consulta financiación</button>
+      <a href={`https://api.whatsapp.com/send?phone=59899673830&text=${probar}`} target='blank' style={{color: 'white', display:'flex', justifyContent: 'center'}} className='BotonesProductoWallpaper' id='BotonesProductoWallpaperWp' > <i class="bi bi-whatsapp"></i> Consulta financiación</a>
       :
       ''
     }
--- src/components/Product/PhoneButtonsProduct.jsx
+++ src/components/Product/PhoneButtonsProduct.jsx
@@ -6,7 +6,7 @@ const PhoneButtonsProduct = (props) => {
         {
             props.producto.Clase==='Motos'
             ?
-            <button onClick={()=>{props.HandleModal()}}  className='BotonesProductoWallpaper' id='BotonesProductoPhoneFinanciacion' ><i class="bi bi-whatsapp"></i>  Consulta financiación</button>
+            <a href={`https://api.whatsapp.com/send?phone=59899673830&text=Estoy%20interesado/a%20en%20financiar%20una%20${props.producto.brand}%20${props.producto.model}`} target="_blank" className='BotonesProductoWallpaper' id='BotonesProductoPhoneFinanciacion' ><i class="bi bi-whatsapp"></i>  Consulta financiación</a>
             :''
         }                                
         <button onClick={()=>{props.context.addToCart(props.producto, props.producto.options[props.seleccionado])}} className='BotonesProductoWallpaper' id={props.producto.Clase==='Motos' ? 'BotonesProductoPhoneCarrito' : 'BotonesProductoPhoneFinanciacion'} >Agregar al carrito</button>                                
